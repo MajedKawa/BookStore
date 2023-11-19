c9c6e612c0697df70befd9a6288b28d9e13684a0
MajedKawa <113922396+MajedKawa@users.noreply.github.com>
2023-11-19 19:55:45 +0200

    adding the orders and frontend microservices

---
+++ frontend-microservice/app/Http/Controllers/FrontendController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class FrontendController extends Controller
+{
+public function welcome()
+{
+
+// return a basic welcome message
+return view('welcome', ['message' => 'Welcome to Bazar.com application!']);
+}
+}
